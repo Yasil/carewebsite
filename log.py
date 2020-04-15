83d6e534da264428d08a5242aeefdfb5abf06330
yasil <yasil@github.com>
2020-04-15 11:26:38 +0800

    优化功能和BUG修复

--- log.py
+++ log.py
@@ -1,12 +1,12 @@
 import logging
 
-logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.INFO)
 
-# logging.basicConfig(level=logging.INFO,
-#                     filename='run.log',
-#                     datefmt='%Y/%m/%d %H:%M:%S',
-#                     format='%(asctime)s - %(levelname)s - %(lineno)d - %(module)s: %(message)s'
-#                     )
+logging.basicConfig(level=logging.INFO,
+                    filename='run.log',
+                    datefmt='%Y/%m/%d %H:%M:%S',
+                    format='%(asctime)s - %(levelname)s - %(lineno)d - %(module)s: %(message)s'
+                    )
 
 # logger = logging.getLogger(__name__)
 # logger.info('This is a log info')
